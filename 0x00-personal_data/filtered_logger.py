2df019ae627bc2fa7bc59ada6b4469d47b934e65
Nickson K Rop <nickyrutto@gmail.com>
2024-07-05 23:13:17 -0700

    fix pycoestyle error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,11 +6,13 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     '''
     This function constructs a regex pattern to match the specified
     fields and uses re.sub to replace the values of these fields
     with the redaction string.
     '''
-    pattern = pattern = '|'.join(f"{field}=[^{separator}]*" for field in fields)
-    return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
+    pattern = '|'.join(f"{field}=[^{separator}]*" for field in fields)
+    return re.sub(pattern,
+                  lambda m: f"{m.group().split('=')[0]}={redaction}", message)
