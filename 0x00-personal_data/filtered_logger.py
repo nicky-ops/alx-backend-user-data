d7d4eef43fc3ce6f903a6972e4c74676167586ee
Nickson K Rop <nickyrutto@gmail.com>
2024-07-05 23:39:29 -0700

    lof formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,28 @@ This module implements message logging
 '''
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''
+        This function filters values in incoming log records
+        '''
+        original_message = super().format(record)
+        return filter_datum(self.fields,
+                            self.REDACTION, original_message, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,
