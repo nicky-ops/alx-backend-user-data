62783272264168199585ae12130d637ee51a24e1
Nickson K Rop <nickyrutto@gmail.com>
2024-07-05 21:56:04 -0700

    obfuscating some fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,13 +2,14 @@
 '''
 This module implements message logging
 '''
-import logger
+import re
 
 
-logging.basicConfig(fields=[''])
-
-
-def filter_datum(fields, redaction, message, seperator):
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
     '''
-    This function returns the log message obfuscated
+    This function constructs a regex pattern to match the specified
+    fields and uses re.sub to replace the values of these fields
+    with the redaction string.
     '''
+    pattern = '|'.join(f"{field}=.*?(?={separator}|$)" for field in fields)
+    return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
