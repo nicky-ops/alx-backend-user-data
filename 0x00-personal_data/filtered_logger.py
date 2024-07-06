5519ac4d729eff59b6c66f3efe836f4a9c8e9283
Nickson K Rop <nickyrutto@gmail.com>
2024-07-05 22:01:07 -0700

    add type annnotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,11 +5,11 @@ This module implements message logging
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: list[str], redaction: str, message: str, separator: str) -> str:
     '''
     This function constructs a regex pattern to match the specified
     fields and uses re.sub to replace the values of these fields
     with the redaction string.
     '''
-    pattern = '|'.join(f"{field}=.*?(?={separator}|$)" for field in fields)
+    pattern = pattern = '|'.join(f"{field}=[^{separator}]*" for field in fields)
     return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
