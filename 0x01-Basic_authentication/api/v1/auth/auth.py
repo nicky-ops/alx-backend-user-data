0029e5e5c3286fd33403a2765c79f20f627d6367
Nickson K Rop <nickyrutto@gmail.com>
2024-07-09 14:14:13 -0700

    fix pycodestyle error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,13 +16,11 @@ class Auth:
         '''
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """
         HTTP authorization header
         """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
