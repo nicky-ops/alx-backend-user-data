c65857760b54a65359db4e53ebfe59b1fca9f733
Nickson K Rop <nickyrutto@gmail.com>
2024-07-10 04:38:39 -0700

    request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE')
+
+if auth_type:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -35,6 +41,22 @@ def not_allowed(error) -> str:
     403 handler
     '''
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    '''
+    This method handles before_request
+    '''
+    if auth is None:
+        return
+    exempt_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, exempt_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
