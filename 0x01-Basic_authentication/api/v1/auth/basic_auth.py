26b6d23b70f7c16f00ea10794189fb506a93be6b
Nickson K Rop <nickyrutto@gmail.com>
2024-07-10 07:30:35 -0700

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Basic authentication
+'''
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    '''
+    This class defines Basic authentication
+    '''
+    pass
