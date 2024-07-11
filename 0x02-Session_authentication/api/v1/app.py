bc1f1590857101e356c20a5692bf23b88cbbcb3f
Nickson K Rop <nickyrutto@gmail.com>
2024-07-11 02:54:26 -0700

    fix syntax erro

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -65,7 +65,8 @@ def before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.session_cookie(request) is None:
+    if auth.session_cookie(request) is None and auth.authorization_header(
+      request) is None:
         abort(401)
 
     request.current_user = auth.current_user(request)
