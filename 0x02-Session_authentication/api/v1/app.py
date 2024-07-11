1c41b8456a304fb7b10c3711e63b60b9e29b8cd9
Nickson K Rop <nickyrutto@gmail.com>
2024-07-11 02:41:46 -0700

    updating before reques method

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,13 +57,16 @@ def before_request():
     if auth is None:
         return
     exempt_paths = ['/api/v1/status/',
-                    '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                    '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                    '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, exempt_paths):
         return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    if auth.session_cookie(request) is None:
+        abort(401)
 
     request.current_user = auth.current_user(request)
 
