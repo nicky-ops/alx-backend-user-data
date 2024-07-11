caf25da6f292dde0137ba1a5345429433074ce60
Nickson K Rop <nickyrutto@gmail.com>
2024-07-11 02:00:10 -0700

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,21 @@
 Session authentication
 '''
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     '''
     This class implements session based authentication
     '''
-    pass
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        '''
+        This function creates a session and returns the session ID
+        '''
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
