3d8e5727bc9e800144c912ebf98a5b263cb88b7f
Nickson K Rop <nickyrutto@gmail.com>
2024-07-15 06:38:54 -0700

    method to upate user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,3 +53,15 @@ class DB:
             raise NoResultFound
         except InvalidRequestError:
             raise InvalidRequestError
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        '''
+        This function updates a user based on their id
+        '''
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+
