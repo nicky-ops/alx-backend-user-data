5c13e66a719556c32dc2d09472fb6f5f0f9aeba4
Nickson K Rop <nickyrutto@gmail.com>
2024-07-16 02:55:42 -0700

    log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,12 +55,13 @@ def logout() -> str:
     '''
     logout function
     '''
-    session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if not user:
+    try:
+        session_id = request.cookies.get('session_id')
+        user = AUTH.get_user_from_session_id(session_id)
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    except:
         abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect("/")
 
 
 if __name__ == "__main__":
